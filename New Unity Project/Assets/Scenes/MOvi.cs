04b338453334f8c395237dc74f3bd579ea26c976
PCCCarmo <lpaulo.cesar.23@gmail.com>
2019-05-21 21:41:08 -0300

    primeiro projeto unity

---
+++ New Unity Project/Assets/Scenes/MOvi.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOvi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float T = Input.GetAxis("Console");
+        float V = Input.GetAxis("Vertical");
+
+        //transform.translate(new Vector3(T * Time.deltatime, V *
+        transform.Translate(new Vector3(T * 20 * Time.deltaTime, 0, 0));
+
+
+        transform.Translate(new Vector3(0, V * 10 * Time.deltaTime, 0));
+
+    }
+}
---
+++ New Unity Project/Assets/Scenes/PersoR.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersoR : MonoBehaviour
+{
+    public float velocidade = 3.0f;
+    public float forcapulo = 600f;
+    public bool direita = true;
+    public float moveX;
+    private Rigidbody2D objeto;
+    private Transform transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objeto = GetComponent<Rigidbody2D>();
+        transform = GetComponent<Transform>();
+
+    }
+
+    void Mover()
+    {
+        moveX = Input.GetAxis("Console");
+        if (moveX > 0 && !direita)
+        {
+            virarPersonagem();
+        }
+        else if (moveX < 0 && direita)
+        {
+            virarPersonagem();
+        }
+        objeto.velocity = new Vector2(moveX * velocidade, objeto.velocity.y);
+    }
+    void virarPersonagem()
+    {
+        direita = !direita;
+        Vector3 scale = GetComponent<Transform>().localScale;
+        scale.x *= -1;
+        GetComponent<Transform>().localScale = scale;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Mover();
+    }
+}
